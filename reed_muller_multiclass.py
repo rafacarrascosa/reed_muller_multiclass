9b03421a27122817c41f396d633c403cae007eb3
Rafael Carrascosa <rafacarrascosa@gmail.com>
2020-06-28 23:10:16 -0300

    use numpy as np instead

--- reed_muller_multiclass.py
+++ reed_muller_multiclass.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 
 def _build_square(A, B, C, D):
@@ -6,9 +6,9 @@ def _build_square(A, B, C, D):
     A B
     C D
     """
-    return numpy.vstack((
-        numpy.hstack((A, B)),
-        numpy.hstack((C, D))
+    return np.vstack((
+        np.hstack((A, B)),
+        np.hstack((C, D))
     ))
 
 
@@ -19,7 +19,7 @@ def _reed_muller_trivial(m):
     if m < 1:
         raise ValueError("m cannot be less than 1")
     if m == 1:
-        return numpy.array([
+        return np.array([
             [1, 1],
             [0, 1]
         ])
@@ -28,7 +28,7 @@ def _reed_muller_trivial(m):
     return _build_square(
         smaller,
         smaller,
-        numpy.zeros((2 ** mm, 2 ** mm), dtype=int),
+        np.zeros((2 ** mm, 2 ** mm), dtype=int),
         smaller
     )
 
@@ -46,21 +46,21 @@ def reed_muller(r, m):
         return _build_square(
             smaller,
             smaller,
-            numpy.zeros((1, 2 ** mm), dtype=int),
-            numpy.ones((1, 2 ** mm), dtype=int),
+            np.zeros((1, 2 ** mm), dtype=int),
+            np.ones((1, 2 ** mm), dtype=int),
         )
     top = reed_muller(r, mm)
     bot = reed_muller(r - 1, mm)
     return _build_square(
         top,
         top,
-        numpy.zeros((bot.shape[0], 2 ** mm), dtype=int),
+        np.zeros((bot.shape[0], 2 ** mm), dtype=int),
         bot
     )
 
 
 def _binary_space_matrix(n, bits):
-    result = numpy.unpackbits(numpy.arange(n, dtype=">u4").view("uint8"))
+    result = np.unpackbits(np.arange(n, dtype=">u4").view("uint8"))
     return result.reshape((n, -1))[:, -bits:]
 
 
@@ -83,11 +83,11 @@ class ReedMullerCodec:
         return self.codewords[i]
 
     def decode_log_proba(self, block):
-        p = numpy.array(block, dtype=float)
+        p = np.array(block, dtype=float)
         if (p < 0).any() or (p > 1).any():
             raise ValueError("block must be numbers between 0 and 1")
         scores = self.codewords * p + (1 - self.codewords) * (1 - p)
-        scores = numpy.log(scores + 1e-11)
+        scores = np.log(scores + 1e-11)
         return scores.sum(axis=1)
 
     def decode(self, block):
